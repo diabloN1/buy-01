3a281f6dda3b0722bbccad1560b0e93e94a9544d
myacoubi <amineamoune904@gmail.com>
2026-07-16 17:08:57 +0100

    feat(product-service): add Exception handlers

---
+++ product-service/src/main/java/com/buy01/product/exception/GlobalExceptionHandler.java
@@ -0,0 +1,123 @@
+package com.buy01.product.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import com.buy01.product.exception.custom.BadRequestException;
+import com.buy01.product.exception.custom.ConflictException;
+import com.buy01.product.exception.custom.ForbiddenException;
+import com.buy01.product.exception.custom.NotFoundException;
+import com.buy01.product.exception.custom.UnauthorizedException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, String>> handleValidation(
+                        MethodArgumentNotValidException ex) {
+
+                Map<String, String> errors = new HashMap<>();
+
+                ex.getBindingResult()
+                                .getFieldErrors()
+                                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(errors);
+        }
+
+        @ExceptionHandler(BadRequestException.class)
+        public ResponseEntity<?> handleBadRequest(BadRequestException ex) {
+
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(Map.of(
+                                                "error", ex.getMessage()));
+        }
+
+        @ExceptionHandler(NotFoundException.class)
+        public ResponseEntity<?> handleNotFound(NotFoundException ex) {
+
+                return ResponseEntity
+                                .status(HttpStatus.NOT_FOUND)
+                                .body(Map.of(
+                                                "error", ex.getMessage()));
+        }
+
+        @ExceptionHandler(UnauthorizedException.class)
+        public ResponseEntity<?> handleUnauthorized(UnauthorizedException ex) {
+
+                return ResponseEntity
+                                .status(HttpStatus.UNAUTHORIZED)
+                                .body(Map.of(
+                                                "error", ex.getMessage()));
+        }
+
+        @ExceptionHandler(HttpMessageNotReadableException.class)
+        public ResponseEntity<?> handleInvalidJson(HttpMessageNotReadableException ex) {
+
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(Map.of(
+                                                "error", "Invalid JSON format",
+                                                "details", ex.getMostSpecificCause() != null
+                                                                ? ex.getMostSpecificCause().getMessage()
+                                                                : ex.getMessage()));
+        }
+
+        @ExceptionHandler(NoResourceFoundException.class)
+        public ResponseEntity<?> handleInvalidRoute(NoResourceFoundException ex) {
+
+                return ResponseEntity
+                                .status(HttpStatus.NOT_FOUND)
+                                .body(Map.of(
+                                                "error", ex.getMessage()));
+        }
+
+        @ExceptionHandler(ConflictException.class)
+        public ResponseEntity<?> handleConflict(ConflictException ex) {
+                return ResponseEntity
+                                .status(HttpStatus.CONFLICT)
+                                .body(Map.of(
+                                                "error", ex.getMessage()));
+        }
+
+        @ExceptionHandler(ForbiddenException.class)
+        public ResponseEntity<?> handleForbidden(ForbiddenException ex) {
+                return ResponseEntity
+                                .status(HttpStatus.FORBIDDEN)
+                                .body(Map.of(
+                                                "error", ex.getMessage()));
+        }
+        
+
+        @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+        public ResponseEntity<?> handleMethodNotAllowed(HttpRequestMethodNotSupportedException ex) {
+                return ResponseEntity
+                                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                                .body(Map.of(
+                                                "error", ex.getMessage()));
+        }
+
+        // Catch All
+        @ExceptionHandler(Exception.class)
+        public ResponseEntity<?> handleGenericException(Exception ex) {
+
+                System.out.println("Error 500: " + ex.getMessage());
+
+                return ResponseEntity
+                                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body(Map.of(
+                                                "error", "Internal Server Error"));
+        }
+}
