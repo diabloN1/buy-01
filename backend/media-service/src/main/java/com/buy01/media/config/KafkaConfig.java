1ef7b4aedbeb548c2db1daaedcc4091fbc9b3ce3
myacoubi <amineamoune904@gmail.com>
2026-07-24 05:55:59 +0100

    feat: enable Kafka support in user and media services, add Kafka configs

--- backend/media-service/src/main/java/com/buy01/media/config/KafkaConfig.java
+++ backend/media-service/src/main/java/com/buy01/media/config/KafkaConfig.java
@@ -3,13 +3,14 @@ package com.buy01.media.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -24,32 +25,36 @@ public class KafkaConfig {
     private String kafkaServers;
 
     @Bean
+    NewTopic avatarDeletedTopic() {
+        return TopicBuilder.name("avatar-deleted")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
     ProducerFactory<String, ImageDeletedEvent> producerFactory() {
 
         Map<String, Object> config = new HashMap<>();
 
         config.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                kafkaServers
-        );
+                kafkaServers);
 
         config.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class
-        );
+                StringSerializer.class);
 
         config.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JacksonJsonSerializer.class
-        );
+                JacksonJsonSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
 
     @Bean
     KafkaTemplate<String, ImageDeletedEvent> kafkaTemplate(
-            ProducerFactory<String, ImageDeletedEvent> producerFactory
-    ) {
+            ProducerFactory<String, ImageDeletedEvent> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
 }
--- backend/user-service/src/main/java/com/__buy/user_service/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/__buy/user_service/UserServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableMongoAuditing
 @SpringBootApplication
 @EnableFeignClients
+@EnableKafka
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
