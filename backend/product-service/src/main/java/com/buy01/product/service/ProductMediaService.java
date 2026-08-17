ceecb71cfdefbd31fceb07ca814e1af724adc9dc
myacoubi <amineamoune904@gmail.com>
2026-08-17 15:47:02 +0100

    bug: sync ProductMediaServicetest with its impl

--- backend/product-service/src/main/java/com/buy01/product/service/ProductMediaService.java
+++ backend/product-service/src/main/java/com/buy01/product/service/ProductMediaService.java
@@ -35,6 +35,6 @@ public class ProductMediaService {
             return;
         }
 
-        imageIds.forEach(id -> mediaClient.delete(id, true));
+        imageIds.forEach(id -> mediaClient.delete(id));
     }
 }
--- backend/todo.md
+++ backend/todo.md
@@ -1,1 +1,2 @@
 [ ] - Remove update post for admin
+[ ] - Send userId and Role only from api-gateway to other services
